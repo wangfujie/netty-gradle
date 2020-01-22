a90972f207031105f4820c5cff1c65c5fa875467
wangfj <wangfj@tongtech.com>
2020-01-22 16:46:44 +0800

    update netty socket2

--- src/main/java/com/wangfj/netty/socketexample/client/MyClient.java
+++ src/main/java/com/wangfj/netty/socketexample/client/MyClient.java
@@ -5,14 +5,21 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.EventLoopGroup;
+import io.netty.channel.epoll.EpollSocketChannel;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
 import io.netty.handler.codec.LengthFieldPrepender;
+import io.netty.handler.codec.bytes.ByteArrayDecoder;
+import io.netty.handler.codec.bytes.ByteArrayEncoder;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
 import io.netty.util.CharsetUtil;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 
 /**
  * 客户端
@@ -28,23 +35,32 @@ public class MyClient {
         try {
             Bootstrap bootstrap = new Bootstrap();
             bootstrap.group(eventLoopGroup).channel(NioSocketChannel.class)
+            //bootstrap.group(eventLoopGroup).channel(EpollSocketChannel.class)
             //客户端初始化器
             .handler(new ChannelInitializer<SocketChannel>() {
                 @Override
                 protected void initChannel(SocketChannel ch) throws Exception {
                     ChannelPipeline pipeline = ch.pipeline();
-                    //添加解码器
-                    pipeline.addLast(new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE, 0,4,0,4));
-                    pipeline.addLast(new LengthFieldPrepender(4));
-                    pipeline.addLast(new StringDecoder(CharsetUtil.UTF_8));
-                    pipeline.addLast(new StringEncoder(CharsetUtil.UTF_8));
+                    //解码 Decoders
+                    pipeline.addLast("frameDecoder", new LengthFieldBasedFrameDecoder(1048576, 0, 4, 0, 4));
+                    pipeline.addLast("bytesDecoder", new ByteArrayDecoder());
+
+                    //编码 Encoder
+                    pipeline.addLast("frameEncoder", new LengthFieldPrepender(4));
+                    pipeline.addLast("bytesEncoder", new ByteArrayEncoder());
+
+                    //添加字符串解码编码处理
+                    //pipeline.addLast(new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE, 0,4,0,4));
+                    //pipeline.addLast(new LengthFieldPrepender(4));
+                    //pipeline.addLast(new StringDecoder(CharsetUtil.UTF_8));
+                    //pipeline.addLast(new StringEncoder(CharsetUtil.UTF_8));
                     //自定义处理类
                     pipeline.addLast(new MyClientHandler());
                 }
             });
 
             //连接服务端
-            ChannelFuture channelFuture = bootstrap.connect("localhost", 8899).sync();
+            ChannelFuture channelFuture = bootstrap.connect("127.0.0.1", 10301).sync();
             channelFuture.channel().closeFuture().sync();
         }finally {
             eventLoopGroup.shutdownGracefully();
